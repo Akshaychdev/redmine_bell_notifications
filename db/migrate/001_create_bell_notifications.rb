b52e7437421be394da91f2e481d9f6efac9b61d2
Akshaychdev <akshaych.dev@gmail.com>
2025-12-26 19:32:59 +0530

    fix: Update author information and retention period in plugin settings

--- db/migrate/001_create_bell_notifications.rb
+++ db/migrate/001_create_bell_notifications.rb
@@ -1,16 +1,22 @@
 class CreateBellNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :bell_notifications do |t|
-      t.references :user, null: false, foreign_key: true, index: true
-      t.references :notifiable, polymorphic: true, index: true
+      t.integer :user_id, null: false
+      t.integer :notifiable_id
+      t.string :notifiable_type
       t.string :event_type, null: false
       t.integer :actor_id
       t.string :title, null: false
       t.text :body
       t.string :url
-      t.datetime :read_at, index: true
+      t.datetime :read_at
       t.timestamps
     end
+
+    # Add foreign key manually with correct type
+    add_foreign_key :bell_notifications, :users
+    add_index :bell_notifications, :user_id
+    add_index :bell_notifications, :read_at
 
     # Composite index for efficient unread queries
     add_index :bell_notifications, [:user_id, :read_at, :created_at], name: 'index_bell_notif_user_read_created'
